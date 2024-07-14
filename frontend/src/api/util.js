c81f67f943cd1aed380c5207d25d812c37356e16
WonTaeJang <jwt0212@gmail.com>
2024-07-14 16:51:14 +0900

    build: env 추가

--- frontend/src/api/util.js
+++ frontend/src/api/util.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: `${import.meta.env[`VITE_${import.meta.env['VITE_MODE']}_SERVER`]}`,
+  // baseURL: 'http://jjolim.com',
   timeout: 10000,
 
 })
